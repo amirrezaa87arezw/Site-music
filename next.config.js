66f3185c7c952813c0d56889f1046317652e7e2c
amirrezaa87arezw <amirrezarezvani244@atomicmail.io>
2026-09-02 15:43:37 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  experimental: {
-    serverActions: { bodySizeLimit: "50mb" }
-  }
-};
-
-module.exports = nextConfig;
+experimental: {
+    serverActions: { bodySizeLimit: "60mb" }
+}
